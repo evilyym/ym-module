18127cee20928f8d3089ee879babfe49ffcb4e12
yym <yangym@golive.cc>
2024-04-11 17:28:34 +0800

    chore  完成

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import VocBtn from './app.vue'
+// import VocBtn from './app.vue'
 import Format from "./3";
 import Validate from "./2";
 
 export { Format, Validate };
-export default VocBtn;
+// export default VocBtn;
